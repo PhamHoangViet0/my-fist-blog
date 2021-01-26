924c14a7cb74b380364435e624c9e71d71719685
ViNton <fam2000anton@gmail.com>
2021-01-26 17:04:59 +0200

    Fixing

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('accounts/signup/', views.SignUpView.as_view(), name='signup'),
 ]
